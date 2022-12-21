4da4a2cb1536aecbd947b3f4d234ec5a0c8904b5
Ben Beach <bbeach@innovated.tech>
2022-12-20 19:32:18 -0600

    socket working properly

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+require('dotenv').config({path:'/home/socket/public_html/.env'});
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
+
+console.log('Environment DB: ', process.env.PG_DB_NAME)
+
 const io = new Server(server, {
     cors: {
-        origin: process.env.CORS_URL
+        origin: ['https://admin.wcwater.com', 'https://wcadev.innovated.tech']
     }
 });
+
 const { createAdapter } = require("@socket.io/postgres-adapter");
 const { Pool } = require("pg");
 
@@ -58,28 +63,39 @@ app.post('/webhook', (req, res) => {
         direction = 'received';
     }
 
-    let queryFindService = `SELECT id AS client_id, display_name FROM clients_client WHERE main_phone = '${from}' OR main_phone = '${to}'`
+    let queryFindService = `SELECT id AS client_id, display_name
+                            FROM clients_client WHERE
+                                main_phone = $1 OR main_phone = $2 OR
+                                alternate_phone = $1 OR alternate_phone = $2 OR
+                                other_phone = $1 OR other_phone = $2`;
+    let queryFindServiceValues = [from, to]
 
     // console.log(queryFindService);
 
-    pool.query(queryFindService, [], (err, res) => {
+    pool.query(queryFindService, queryFindServiceValues, (err, res) => {
         if(err) throw err;
 
         let db_client = res.rows[0];
-        let queued;
+        let queued, client_id;
 
         queued = !!payload.completed_at;
 
+        if(db_client) {
+            client_id = db_client.client_id;
+        } else {
+            client_id = null;
+        }
+
         let queryText = `
             INSERT INTO communication_textmessage (from_phone, to_phone, message, direction, read, timestamp, media_count, media, client_id, queued, message_uid)
-            VALUES ($1, $2, $3, '${direction}', False, now(), $4, $5, ${db_client.client_id}, ${queued}, $6)
+            VALUES ($1, $2, $3, '${direction}', False, now(), $4, $5, ${client_id}, ${queued}, $6)
             ON CONFLICT (message_uid) DO UPDATE SET queued = ${queued}`;
 
         let queryValues = [from, to, body, num_media, media, message_uid];
 
         pool.query(queryText, queryValues, (err, res) => {
             if(err) throw err;
-            io.emit('received message', payload);
+            io.emit('received message', payload, db_client);
         });
 
         io.emit('update notifications', db_client, payload);
@@ -110,12 +126,22 @@ io.on('connection', (socket) => {
             });
     });
 
-    socket.on('text messages', (phone) => {
-        pool.query(`SELECT * FROM communication_textmessage WHERE from_phone = '${phone}' OR to_phone = '${phone}' ORDER BY timestamp DESC;`,
-            [], (err, res) => {
-                if(err) throw err;
-                io.emit('all text messages', res.rows);
+    socket.on('mark client texts read', (phone) => {
+        if(phone) {
+            let queryUnread = `SELECT * FROM communication_textmessage WHERE from_phone = $1 OR to_phone = $1 AND read = FALSE`;
+            let queryUnreadValues = [phone]
+
+            pool.query(queryUnread, queryUnreadValues, (err, res) => {
+                if(res.rows) {
+                    io.emit('client texts read', res.rows);
+                }
+
+                let queryMessage = `UPDATE communication_textmessage SET read = TRUE WHERE from_phone = $1 OR to_phone = $1 AND read = FALSE`;
+                let queryValues = [phone]
+
+                pool.query(queryMessage, queryValues);
             });
+        }
     });
 });
 
